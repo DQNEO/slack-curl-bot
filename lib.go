00e75700225e113621a832cdf499f8691f046d6e
DQNEO <dqneoo@gmail.com>
2018-04-22 19:48:00 +0900

    improve more

--- lib.go
+++ lib.go
@@ -5,35 +5,61 @@ import (
 	"log"
 	"fmt"
 	"strings"
+	"regexp"
+	"bytes"
 )
 
 func execCurl(input string) string {
 	cmd := textToCmd(input)
-	byts, err := cmd.Output()
+
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
+	outStr, errStr := string(stdout.Bytes()), string(stderr.Bytes())
+	fmt.Printf("out:\n%s\nerr:\n%s\n", outStr, errStr)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("cmd.Run() failed with %s\n", err)
 	}
-	out := string(byts)
-	fmt.Printf(out)
-	return out
+
+	return outStr
 }
 
 func isCurlCommand(text string) bool {
-	return strings.HasPrefix(text, "curl ")
+	return strings.HasPrefix(text, "curl")
 }
 
 var BotId = "@UA5UWB2NB"
 
 func isMentionToMe(input string) bool {
-	return strings.HasPrefix(input, fmt.Sprintf("<%s> ", BotId))
+	return strings.HasPrefix(input, fmt.Sprintf("<%s>", BotId))
+}
+
+func removeMention(input string) string {
+	return input[len("<"+BotId+">"):]
 }
 
 func textToCmd(input string) *exec.Cmd {
-	log.Printf("%s\n", input)
-	substr := input[5:len(input)]
+	log.Printf("text:%s\n", input)
+	var substr string
+	if len(input) <= 5 {
+		substr = "--help"
+	} else {
+		substr = input[5:len(input)]
+	}
 
-	cmd := exec.Command("curl", substr)
+	log.Printf("substr:%s\n", substr)
+	arg := unwrapUrl(substr)
+	args := strings.Split(arg, " ")
+	log.Printf("args:%s\n", args)
+	cmd := exec.Command("curl", args...)
 	return cmd
+}
+
+func unwrapUrl(input string) string {
+	re2 := regexp.MustCompile("<(https?://.*)>")
+	output := re2.ReplaceAllString(input, "$1")
+	return output
 }
 
 func handle(input string) string {
@@ -43,6 +69,9 @@ func handle(input string) string {
 		return ""
 	}
 
+	input = removeMention(input)
+	log.Printf(input)
+	input = strings.TrimLeft(input," ")
 	if isCurlCommand(input) {
 		log.Printf("it's curl\n")
 		body := execCurl(input)
